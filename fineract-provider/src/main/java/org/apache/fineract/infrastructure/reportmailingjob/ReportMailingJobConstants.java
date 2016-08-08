apache/fineract

commit for FINERACT-65 (Implement ability to schedule & e-mail reports)

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/ReportMailingJobConstants.java
@@ -1,0 +1,88 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReportMailingJobConstants {
+
+    // define the API resource entity name
+    public static final String REPORT_MAILING_JOB_PERMISSION_NAME = "REPORTMAILINGJOB";
+    
+    // define the API resource name for the report mailing job resource
+    public static final String REPORT_MAILING_JOB_RESOURCE_NAME = "reportmailingjobs";
+    
+    // define the API resource name for the report mailing job history resource
+    public static final String REPORT_MAILING_JOB_RUN_HISTORY_RESOURCE_NAME = "reportmailingjobrunhistory";
+    
+    // general API resource request parameter constants
+    public static final String LOCALE_PARAM_NAME = "locale";
+    public static final String DATE_FORMAT_PARAM_NAME = "dateFormat";
+    
+    // parameter constants for create/update entity API request
+    public static final String NAME_PARAM_NAME = "name";
+    public static final String DESCRIPTION_PARAM_NAME = "description";
+    public static final String START_DATE_TIME_PARAM_NAME = "startDateTime";
+    public static final String RECURRENCE_PARAM_NAME = "recurrence";
+    public static final String EMAIL_RECIPIENTS_PARAM_NAME = "emailRecipients";
+    public static final String EMAIL_SUBJECT_PARAM_NAME = "emailSubject";
+    public static final String EMAIL_MESSAGE_PARAM_NAME = "emailMessage";
+    public static final String EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME = "emailAttachmentFileFormatId";
+    public static final String STRETCHY_REPORT_ID_PARAM_NAME = "stretchyReportId";
+    public static final String STRETCHY_REPORT_PARAM_MAP_PARAM_NAME = "stretchyReportParamMap";
+    public static final String IS_ACTIVE_PARAM_NAME = "isActive";
+    
+    // other parameter constants
+    public static final String ID_PARAM_NAME = "id";
+    public static final String EMAIL_ATTACHMENT_FILE_FORMAT_PARAM_NAME = "emailAttachmentFileFormat";
+    public static final String PREVIOUS_RUN_DATE_TIME_PARAM_NAME = "previousRunDateTime";
+    public static final String NEXT_RUN_DATE_TIME_PARAM_NAME = "nextRunDateTime";
+    public static final String PREVIOUS_RUN_STATUS = "previousRunStatus";
+    public static final String PREVIOUS_RUN_ERROR_LOG = "previousRunErrorLog";
+    public static final String PREVIOUS_RUN_ERROR_MESSAGE = "previousRunErrorMessage";
+    public static final String NUMBER_OF_RUNS = "numberOfRuns";
+    public static final String STRETCHY_REPORT_PARAM_NAME = "stretchyReport";
+    
+    // list of permitted parameters for the create report mailing job request
+    public static final Set<String> CREATE_REQUEST_PARAMETERS = new HashSet<>(Arrays.asList(LOCALE_PARAM_NAME, DATE_FORMAT_PARAM_NAME, 
+            NAME_PARAM_NAME, DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, 
+            EMAIL_MESSAGE_PARAM_NAME, EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME, STRETCHY_REPORT_ID_PARAM_NAME, 
+            STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, START_DATE_TIME_PARAM_NAME));
+    
+    // list of permitted parameters for the update report mailing job request
+    public static final Set<String> UPDATE_REQUEST_PARAMETERS = new HashSet<>(Arrays.asList(LOCALE_PARAM_NAME, DATE_FORMAT_PARAM_NAME, 
+            NAME_PARAM_NAME, DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, 
+            EMAIL_MESSAGE_PARAM_NAME, EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME, STRETCHY_REPORT_ID_PARAM_NAME, 
+            STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, START_DATE_TIME_PARAM_NAME));
+    
+    // list of parameters that represent the properties of a report mailing job
+    public static final Set<String> REPORT_MAILING_JOB_DATA_PARAMETERS = new HashSet<>(Arrays.asList(ID_PARAM_NAME, NAME_PARAM_NAME, 
+            DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, EMAIL_MESSAGE_PARAM_NAME, 
+            EMAIL_ATTACHMENT_FILE_FORMAT_PARAM_NAME, STRETCHY_REPORT_PARAM_NAME, STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, 
+            START_DATE_TIME_PARAM_NAME, PREVIOUS_RUN_DATE_TIME_PARAM_NAME, NEXT_RUN_DATE_TIME_PARAM_NAME, PREVIOUS_RUN_STATUS, 
+            PREVIOUS_RUN_ERROR_LOG, PREVIOUS_RUN_ERROR_MESSAGE, NUMBER_OF_RUNS));
+    
+    // report mailing job configuration names
+    public static final String GMAIL_SMTP_SERVER = "GMAIL_SMTP_SERVER";
+    public static final String GMAIL_SMTP_PORT = "GMAIL_SMTP_PORT";
+    public static final String GMAIL_SMTP_USERNAME = "GMAIL_SMTP_USERNAME";
+    public static final String GMAIL_SMTP_PASSWORD = "GMAIL_SMTP_PASSWORD";
+}

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/handler/DeleteReportMailingJobCommandHandler.java
@@ -1,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob.handler;
+
+import org.apache.fineract.commands.annotation.CommandType;
+import org.apache.fineract.commands.handler.NewCommandSourceHandler;
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.reportmailingjob.ReportMailingJobConstants;
+import org.apache.fineract.infrastructure.reportmailingjob.service.ReportMailingJobWritePlatformService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@CommandType(entity = ReportMailingJobConstants.REPORT_MAILING_JOB_PERMISSION_NAME, action = "DELETE")
+public class DeleteReportMailingJobCommandHandler implements NewCommandSourceHandler {
+    private final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService;
+    
+    @Autowired
+    public DeleteReportMailingJobCommandHandler(final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService) {
+        this.reportMailingJobWritePlatformService = reportMailingJobWritePlatformService;
+    }
+
+    @Override
+    @Transactional
+    public CommandProcessingResult processCommand(JsonCommand jsonCommand) {
+        return this.reportMailingJobWritePlatformService.deleteReportMailingJob(jsonCommand.entityId());
+    }
+}

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/handler/UpdateReportMailingJobCommandHandler.java
@@ -1,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob.handler;
+
+import org.apache.fineract.commands.annotation.CommandType;
+import org.apache.fineract.commands.handler.NewCommandSourceHandler;
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.reportmailingjob.ReportMailingJobConstants;
+import org.apache.fineract.infrastructure.reportmailingjob.service.ReportMailingJobWritePlatformService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@CommandType(entity = ReportMailingJobConstants.REPORT_MAILING_JOB_PERMISSION_NAME, action = "UPDATE")
+public class UpdateReportMailingJobCommandHandler implements NewCommandSourceHandler {
+    private final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService;
+    
+    @Autowired
+    public UpdateReportMailingJobCommandHandler(final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService) {
+        this.reportMailingJobWritePlatformService = reportMailingJobWritePlatformService;
+    }
+
+    @Override
+    @Transactional
+    public CommandProcessingResult processCommand(JsonCommand jsonCommand) {
+        return this.reportMailingJobWritePlatformService.updateReportMailingJob(jsonCommand.entityId(), jsonCommand);
+    }
+}

fineract-provider/src/main/resources/sql/migrations/core_db/V312__report_mailing_job_module.sql
@@ -1,0 +1,58 @@
+create table if not exists m_report_mailing_job (
+id bigint primary key auto_increment,
+name varchar(100) not null,
+description text null,
+start_datetime datetime not null,
+recurrence varchar(100) null,
+created_on_date date not null,
+created_by_userid bigint not null,
+email_recipients text not null,
+email_subject varchar(100) not null,
+email_message text not null,
+email_attachment_file_format varchar(10) not null,
+stretchy_report_id int not null,
+stretchy_report_param_map text null,
+previous_run_datetime datetime null,
+next_run_datetime datetime null,
+previous_run_status varchar(10) null,
+previous_run_error_log text null,
+previous_run_error_message text null,
+number_of_runs int not null default 0,
+is_active tinyint(1) not null default 0,
+is_deleted tinyint(1) not null default 0,
+run_as_userid bigint not null,
+foreign key (created_by_userid) references m_appuser(id),
+foreign key (stretchy_report_id) references stretchy_report(id),
+foreign key (run_as_userid) references m_appuser(id),
+constraint unique_name unique (name)
+);
+
+create table if not exists m_report_mailing_job_run_history (
+id bigint primary key auto_increment,
+job_id bigint not null,
+start_datetime datetime not null,
+end_datetime datetime not null,
+status varchar(10) not null,
+error_message text null,
+error_log text null,
+foreign key (job_id) references m_report_mailing_job (id)
+);
+
+create table if not exists m_report_mailing_job_configuration (
+id int primary key auto_increment,
+name varchar(50) not null,
+`value` varchar(200) not null,
+constraint unique_name unique (name)
+);
+
+insert into m_permission (`grouping`, code, entity_name, action_name, can_maker_checker)
+values ('jobs', 'CREATE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'CREATE', 0), 
+('jobs', 'UPDATE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'UPDATE', 0), 
+('jobs', 'DELETE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'DELETE', 0), 
+('jobs', 'READ_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'READ', 0);
+
+insert into m_report_mailing_job_configuration (name, `value`)
+values ('GMAIL_SMTP_SERVER', 'smtp.gmail.com'), ('GMAIL_SMTP_PORT', 587), ('GMAIL_SMTP_USERNAME', ''), ('GMAIL_SMTP_PASSWORD', '');
+
+insert into job (name, display_name, cron_expression, create_time)
+values ('Execute Report Mailing Jobs', 'Execute Report Mailing Jobs', '0 0/15 * * * ?', NOW());

garfenterdreams/banco

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/ReportMailingJobConstants.java
@@ -1,0 +1,88 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReportMailingJobConstants {
+
+    // define the API resource entity name
+    public static final String REPORT_MAILING_JOB_ENTITY_NAME = "REPORTMAILINGJOB";
+    
+    // define the API resource name for the report mailing job resource
+    public static final String REPORT_MAILING_JOB_RESOURCE_NAME = "reportmailingjobs";
+    
+    // define the API resource name for the report mailing job history resource
+    public static final String REPORT_MAILING_JOB_RUN_HISTORY_RESOURCE_NAME = "reportmailingjobrunhistory";
+    
+    // general API resource request parameter constants
+    public static final String LOCALE_PARAM_NAME = "locale";
+    public static final String DATE_FORMAT_PARAM_NAME = "dateFormat";
+    
+    // parameter constants for create/update entity API request
+    public static final String NAME_PARAM_NAME = "name";
+    public static final String DESCRIPTION_PARAM_NAME = "description";
+    public static final String START_DATE_TIME_PARAM_NAME = "startDateTime";
+    public static final String RECURRENCE_PARAM_NAME = "recurrence";
+    public static final String EMAIL_RECIPIENTS_PARAM_NAME = "emailRecipients";
+    public static final String EMAIL_SUBJECT_PARAM_NAME = "emailSubject";
+    public static final String EMAIL_MESSAGE_PARAM_NAME = "emailMessage";
+    public static final String EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME = "emailAttachmentFileFormatId";
+    public static final String STRETCHY_REPORT_ID_PARAM_NAME = "stretchyReportId";
+    public static final String STRETCHY_REPORT_PARAM_MAP_PARAM_NAME = "stretchyReportParamMap";
+    public static final String IS_ACTIVE_PARAM_NAME = "isActive";
+    
+    // other parameter constants
+    public static final String ID_PARAM_NAME = "id";
+    public static final String EMAIL_ATTACHMENT_FILE_FORMAT_PARAM_NAME = "emailAttachmentFileFormat";
+    public static final String PREVIOUS_RUN_DATE_TIME_PARAM_NAME = "previousRunDateTime";
+    public static final String NEXT_RUN_DATE_TIME_PARAM_NAME = "nextRunDateTime";
+    public static final String PREVIOUS_RUN_STATUS = "previousRunStatus";
+    public static final String PREVIOUS_RUN_ERROR_LOG = "previousRunErrorLog";
+    public static final String PREVIOUS_RUN_ERROR_MESSAGE = "previousRunErrorMessage";
+    public static final String NUMBER_OF_RUNS = "numberOfRuns";
+    public static final String STRETCHY_REPORT_PARAM_NAME = "stretchyReport";
+    
+    // list of permitted parameters for the create report mailing job request
+    public static final Set<String> CREATE_REQUEST_PARAMETERS = new HashSet<>(Arrays.asList(LOCALE_PARAM_NAME, DATE_FORMAT_PARAM_NAME, 
+            NAME_PARAM_NAME, DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, 
+            EMAIL_MESSAGE_PARAM_NAME, EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME, STRETCHY_REPORT_ID_PARAM_NAME, 
+            STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, START_DATE_TIME_PARAM_NAME));
+    
+    // list of permitted parameters for the update report mailing job request
+    public static final Set<String> UPDATE_REQUEST_PARAMETERS = new HashSet<>(Arrays.asList(LOCALE_PARAM_NAME, DATE_FORMAT_PARAM_NAME, 
+            NAME_PARAM_NAME, DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, 
+            EMAIL_MESSAGE_PARAM_NAME, EMAIL_ATTACHMENT_FILE_FORMAT_ID_PARAM_NAME, STRETCHY_REPORT_ID_PARAM_NAME, 
+            STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, START_DATE_TIME_PARAM_NAME));
+    
+    // list of parameters that represent the properties of a report mailing job
+    public static final Set<String> REPORT_MAILING_JOB_DATA_PARAMETERS = new HashSet<>(Arrays.asList(ID_PARAM_NAME, NAME_PARAM_NAME, 
+            DESCRIPTION_PARAM_NAME, RECURRENCE_PARAM_NAME, EMAIL_RECIPIENTS_PARAM_NAME, EMAIL_SUBJECT_PARAM_NAME, EMAIL_MESSAGE_PARAM_NAME, 
+            EMAIL_ATTACHMENT_FILE_FORMAT_PARAM_NAME, STRETCHY_REPORT_PARAM_NAME, STRETCHY_REPORT_PARAM_MAP_PARAM_NAME, IS_ACTIVE_PARAM_NAME, 
+            START_DATE_TIME_PARAM_NAME, PREVIOUS_RUN_DATE_TIME_PARAM_NAME, NEXT_RUN_DATE_TIME_PARAM_NAME, PREVIOUS_RUN_STATUS, 
+            PREVIOUS_RUN_ERROR_LOG, PREVIOUS_RUN_ERROR_MESSAGE, NUMBER_OF_RUNS));
+    
+    // report mailing job configuration names
+    public static final String GMAIL_SMTP_SERVER = "GMAIL_SMTP_SERVER";
+    public static final String GMAIL_SMTP_PORT = "GMAIL_SMTP_PORT";
+    public static final String GMAIL_SMTP_USERNAME = "GMAIL_SMTP_USERNAME";
+    public static final String GMAIL_SMTP_PASSWORD = "GMAIL_SMTP_PASSWORD";
+}

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/handler/DeleteReportMailingJobCommandHandler.java
@@ -1,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob.handler;
+
+import org.apache.fineract.commands.annotation.CommandType;
+import org.apache.fineract.commands.handler.NewCommandSourceHandler;
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.reportmailingjob.ReportMailingJobConstants;
+import org.apache.fineract.infrastructure.reportmailingjob.service.ReportMailingJobWritePlatformService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@CommandType(entity = ReportMailingJobConstants.REPORT_MAILING_JOB_ENTITY_NAME, action = "DELETE")
+public class DeleteReportMailingJobCommandHandler implements NewCommandSourceHandler {
+    private final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService;
+    
+    @Autowired
+    public DeleteReportMailingJobCommandHandler(final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService) {
+        this.reportMailingJobWritePlatformService = reportMailingJobWritePlatformService;
+    }
+
+    @Override
+    @Transactional
+    public CommandProcessingResult processCommand(JsonCommand jsonCommand) {
+        return this.reportMailingJobWritePlatformService.deleteReportMailingJob(jsonCommand.entityId());
+    }
+}

fineract-provider/src/main/java/org/apache/fineract/infrastructure/reportmailingjob/handler/UpdateReportMailingJobCommandHandler.java
@@ -1,0 +1,46 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.fineract.infrastructure.reportmailingjob.handler;
+
+import org.apache.fineract.commands.annotation.CommandType;
+import org.apache.fineract.commands.handler.NewCommandSourceHandler;
+import org.apache.fineract.infrastructure.core.api.JsonCommand;
+import org.apache.fineract.infrastructure.core.data.CommandProcessingResult;
+import org.apache.fineract.infrastructure.reportmailingjob.ReportMailingJobConstants;
+import org.apache.fineract.infrastructure.reportmailingjob.service.ReportMailingJobWritePlatformService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@CommandType(entity = ReportMailingJobConstants.REPORT_MAILING_JOB_ENTITY_NAME, action = "UPDATE")
+public class UpdateReportMailingJobCommandHandler implements NewCommandSourceHandler {
+    private final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService;
+    
+    @Autowired
+    public UpdateReportMailingJobCommandHandler(final ReportMailingJobWritePlatformService reportMailingJobWritePlatformService) {
+        this.reportMailingJobWritePlatformService = reportMailingJobWritePlatformService;
+    }
+
+    @Override
+    @Transactional
+    public CommandProcessingResult processCommand(JsonCommand jsonCommand) {
+        return this.reportMailingJobWritePlatformService.updateReportMailingJob(jsonCommand.entityId(), jsonCommand);
+    }
+}

fineract-provider/src/main/resources/sql/migrations/core_db/V312__report_mailing_job_module.sql
@@ -1,0 +1,61 @@
+create table if not exists m_report_mailing_job (
+id bigint primary key auto_increment,
+name varchar(100) not null,
+description text null,
+start_datetime datetime not null,
+recurrence varchar(100) null,
+created_date date not null,
+createdby_id bigint not null,
+lastmodified_date date null,
+lastmodifiedby_id bigint null,
+email_recipients text not null,
+email_subject varchar(100) not null,
+email_message text not null,
+email_attachment_file_format varchar(10) not null,
+stretchy_report_id int not null,
+stretchy_report_param_map text null,
+previous_run_datetime datetime null,
+next_run_datetime datetime null,
+previous_run_status varchar(10) null,
+previous_run_error_log text null,
+previous_run_error_message text null,
+number_of_runs int not null default 0,
+is_active tinyint(1) not null default 0,
+is_deleted tinyint(1) not null default 0,
+run_as_userid bigint not null,
+foreign key (createdby_id) references m_appuser(id),
+foreign key (lastmodifiedby_id) references m_appuser(id),
+foreign key (stretchy_report_id) references stretchy_report(id),
+foreign key (run_as_userid) references m_appuser(id),
+constraint unique_name unique (name)
+);
+
+create table if not exists m_report_mailing_job_run_history (
+id bigint primary key auto_increment,
+job_id bigint not null,
+start_datetime datetime not null,
+end_datetime datetime not null,
+status varchar(10) not null,
+error_message text null,
+error_log text null,
+foreign key (job_id) references m_report_mailing_job (id)
+);
+
+create table if not exists m_report_mailing_job_configuration (
+id int primary key auto_increment,
+name varchar(50) not null,
+`value` varchar(200) not null,
+constraint unique_name unique (name)
+);
+
+insert into m_permission (`grouping`, code, entity_name, action_name, can_maker_checker)
+values ('jobs', 'CREATE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'CREATE', 0), 
+('jobs', 'UPDATE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'UPDATE', 0), 
+('jobs', 'DELETE_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'DELETE', 0), 
+('jobs', 'READ_REPORTMAILINGJOB', 'REPORTMAILINGJOB', 'READ', 0);
+
+insert into m_report_mailing_job_configuration (name, `value`)
+values ('GMAIL_SMTP_SERVER', 'smtp.gmail.com'), ('GMAIL_SMTP_PORT', 587), ('GMAIL_SMTP_USERNAME', ''), ('GMAIL_SMTP_PASSWORD', '');
+
+insert into job (name, display_name, cron_expression, create_time)
+values ('Execute Report Mailing Jobs', 'Execute Report Mailing Jobs', '0 0/15 * * * ?', NOW());